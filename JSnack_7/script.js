3e5b568bc18f72f7f6db6f565f48adccc778f030
Vale2197 <valentinatrebbi97@gmail.com>
2021-10-19 09:54:09 +0200

    correzione es. Jsnack_7
    sistemato potenze

--- JSnack_7/script.js
+++ JSnack_7/script.js
@@ -1,10 +1,18 @@
 /* Stampa le potenze di 2 fino a 1000. */
 
 /* num da 1 a 1000 */
+let number = 0;
 
 for (let i = 0; i <= 1000; i++) {
     
-    let element = Math.pow(2, i);
-    console.log(element);  
-    document.querySelector(".container").innerHTML += `${element} <br>`
+    number += Math.pow(2, i);
+    
+    if (number <= 1000) {
+        console.log(number);
+        document.querySelector(".container").innerHTML += `${number} <br>`;
+    }
+     
 }
+
+
+
